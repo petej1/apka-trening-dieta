545f2350ec861d14baccb400aab119e223819cfe
Pawel <pawelek1227@gmail.com>
2017-04-08 10:05:03 +0200

    "wizualny panel logowania"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,15 +2,24 @@ package sample;
 
 import javafx.application.Application;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+
 
 public class Main extends Application {
 
 
-    Button button;
+
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,15 +27,38 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Tytul okna");
-        button = new Button("Kliknij mnie!");
+        primaryStage.setTitle("Ekran logowania");
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
 
-        Scene scene = new Scene(layout,300,250);
-        primaryStage.setScene(scene);
         primaryStage.show();
+
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Scene scene = new Scene(grid, 400, 275);
+        primaryStage.setScene(scene);
+
+        Text scenetitle = new Text("Witaj");
+        grid.add(scenetitle,0,0,2,1);
+
+        Label userName = new Label("Login użytkownika");
+        grid.add(userName,0,1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField,1,1);
+
+        Label pw = new Label("Password: ");
+        grid.add(pw,0,2);
+
+        PasswordField pwField = new PasswordField();
+        grid.add(pwField,1,2);
+
+        Button ok = new Button("Press me");
+        grid.add(ok,1,3);
+
     }
 
 
